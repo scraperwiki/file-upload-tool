d48658fe5f072e6edf3dc73ae18e407af551185e
Peter Waller <peter@scraperwiki.com>
2014-01-22 11:29:50 +0000

    Work in progress for 'select report types'

--- http/code.js
+++ http/code.js
@@ -1,3 +1,31 @@
+var showUserFilterChoice = function() {
+  $("#filter-choice").show()
+  
+}
+
+var onFilterSelection = function() {
+  $("#filter-choice").hide()
+  showUploadButton()
+}
+$("#filter-select").on("change", onFilterSelection)
+
+var showUploadButton = function() {
+  $("#file-upload").show()
+}
+
+var showUploadAnotherButton = function() {
+  $("#file-uploaded").show()
+}
+  
+var errorGenericNetworkProblem = function(msg, jqXHR, textStatus, errorThrown) {
+  // Otherwise, it's an unknown error and we don't (can't) know what to do.
+  // Show the user an error and ask them to try again later or report it
+  // if it is persistent.
+  scraperwiki.alert("Oops, something went wrong.",
+    msg+" Try again later and please report it "+
+    "if the problem persists. Status: " + jqXHR.status, "error")
+}
+
 $(function(){
 
   // set up special form inputs
@@ -16,9 +44,47 @@ $(function(){
   // if we've just received a new file
   var filePath = scraperwiki.readSettings().filePath
   if (filePath) {
-    $('#content').prepend('<p>Uploaded file: <b>' + filePath + '</b></p>')
-    $('#upload-button').html('Upload another file')
+    showUploadAnotherButton()
   }
-
+  
+  $.ajax({"url": "settin1gs.json", "dataType": "json"})
+    .done(function(data) {
+      if ("filter" in data) {
+        showUploadButton()
+        return
+      }
+      showUserFilterChoice()
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      if (jqXHR.status == 404) {
+        // if settings.json doesn't exist yet, the user hasn't made a choice.
+        showUserFilterChoice()
+        return
+      }
+      var m = "Settings couldn't be loaded."
+      errorGenericNetworkProblem(m, jqXHR, textStatus, errorThrown)
+    })
+    
+  $.ajax({"url": "filters.json", "dataType": "json"})
+    .done(function(data) {      
+      var $select = $("#filter-select")
+      $select.find("option")
+      
+      // Remove placeholder "Loading...", from the HTML.
+      $select
+        .empty()
+        .html("<option></option>")
+      
+      $.each(data, function(key, value) {
+        $select.append($('<option>').val(key).text(value))
+      })
+      
+      $select.attr("disabled", false)
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      var m = "Available filters could not be loaded."
+      errorGenericNetworkProblem(m, jqXHR, textStatus, errorThrown)
+    })
+    
 })
 
