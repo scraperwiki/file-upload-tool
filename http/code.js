1f67ae0d023d4cdf9ae459110a0840b5394cce41
Zarino Zappia <mail@zarino.co.uk>
2014-01-28 16:57:15 +0000

    a load of automatic whitespace fixes

--- http/code.js
+++ http/code.js
@@ -7,7 +7,7 @@ scraperwiki.readSettings = function() {
     try {
       // Try the hash from the container, if it has one?
       return JSON.parse(decodeURIComponent(parent.location.hash.substr(1)))
-    } catch (e) {}    
+    } catch (e) {}
   }
   return null
 }
@@ -33,7 +33,7 @@ var showUploadButton = function() {
 var showUploadAnotherButton = function() {
   $("#file-uploaded").show()
 }
-  
+
 var errorGenericNetworkProblem = function(msg, jqXHR, textStatus, errorThrown) {
   // Otherwise, it's an unknown error and we don't (can't) know what to do.
   // Show the user an error and ask them to try again later or report it
@@ -61,7 +61,7 @@ var errorFromRunlog = function(runlogEntry) {
   if (matches) {
     showFormattingError(matches[2])
     return
-  } 
+  }
 
   var regex = /^(TableError)\(['"](.+)['"],\)$/
   var matches = exception.match(regex)
@@ -81,7 +81,7 @@ var errorFromRunlog = function(runlogEntry) {
       msg += " (we detected file type: " + mimeTypeMatches[1] + ")."
     } else {
       msg += "."
-    } 
+    }
     showFormattingError(msg)
     return
   }
@@ -99,19 +99,19 @@ var fetchFiltersAndPopulate = function() {
   return $.ajax({
     "url": "filters.json",
     "dataType": "json"
-  }).done(function(data) {      
+  }).done(function(data) {
     var $select = $("#filter-select")
     $select.find("option")
-    
+
     // Remove placeholder "Loading...", from the HTML.
     $select
       .empty()
       .html("<option></option>")
-    
+
     $.each(data, function(key, value) {
       $select.append($('<option>').val(key).text(value))
     })
-    
+
     $select.attr("disabled", false)
   }).fail(function(jqXHR, textStatus, errorThrown) {
     var m = "Available filters could not be loaded."
@@ -139,23 +139,23 @@ var fetchSettings = function() {
 
 var fetchLastRunlogEntry = function() {
   var dfd = $.Deferred()
-  
+
   var q = "SELECT * from _sw_runlog ORDER BY time DESC LIMIT 1"
   scraperwiki.dataset.sql(q).done(function(data) {
     dfd.resolve(data[0])
-    
+
   }).fail(function(jqXHR, textStatus, errorThrown) {
     if (jqXHR.status == 400 || jqXHR.status == 404) {
       dfd.resolve(null)
       return
     }
-    
+
     var m = "Last runlog entry couldn't be loaded."
     errorGenericNetworkProblem(m, jqXHR, textStatus, errorThrown)
-    
+
     dfd.reject(jqXHR, textStatus, errorThrown)
   })
-  
+
   return dfd.promise()
 }
 
@@ -172,9 +172,9 @@ var onFilterSelection = function() {
 
   var $select = $(this)
   var filterName = $select.children("[value='"+$select.val()+"']").text()
-  
+
   scraperwiki.dataset.name(filterName)
-    
+
   $select.attr("disabled", true)
   $("#filter-saving").show()
   $(".filter-name").text(filterName)
@@ -187,15 +187,15 @@ var onFilterSelection = function() {
       filterName: filterName
     },
     dataType: "json"
-    
+
   }).done(function(data) {
     $("#filter-choice").hide()
     showUploadButton()
-    
+
   }).fail(function(jqXHR, textStatus, errorThrown) {
     var m = "Problem saving filter selection."
     errorGenericNetworkProblem(m, jqXHR, textStatus, errorThrown)
-    
+
     $select.attr("disabled", false)
     $("#filter-saving").hide()
   })
@@ -217,7 +217,7 @@ $(function(){
   $('#apikey').val(scraperwiki.readSettings().source.apikey)
 
   // listen for selection of new files
-  $('#file').on('change', onFileUpload)  
+  $('#file').on('change', onFileUpload)
   $("#filter-select").on("change", onFilterSelection)
 
   // listen for clicks on "more details" link in exception messages
@@ -239,24 +239,24 @@ $(function(){
     .done(function(settings, runlogEntry) {
       // Unwrap the 'when'
       settings = settings[0]
-      
+
       if (settings == null || !("filter" in settings)) {
         // Filter hasn't been picked yet
         showUserFilterChoice()
         return
       }
-      
+
       $(".filter-name").text(settings["filterName"])
-      
+
       if (runlogEntry == null) {
         // We've never run before
         showUploadButton()
         return
       }
-      
+
       showUploadAnotherButton()
-      
-      if (runlogEntry.success) {      
+
+      if (runlogEntry.success) {
         if (userJustUploadedSomething) {
           $("#success").show()
         }
