b44ecea27f84528843de5c55a537e406ec5a56d1
Zarino Zappia <mail@zarino.co.uk>
2014-01-29 16:21:50 +0000

    automatic whitespace fixes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Work around pip which does not fetch tags (yet)
-for d in $(find ~/src/ -maxdepth 1 -mindepth 1 -type d); 
-do 
-  cd $d; 
-  git fetch -q --tags; 
+for d in $(find ~/src/ -maxdepth 1 -mindepth 1 -type d);
+do
+  cd $d;
+  git fetch -q --tags;
 done
 
 cd
