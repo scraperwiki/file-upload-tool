f6a66fe990f4140cea353d089503f682a2aceb19
Tom Mortimer-Jones <tom@morty.co.uk>
2014-01-30 11:14:47 +0000

    Install main requirements file when running filter tests.

--- filter/run_tests.sh
+++ filter/run_tests.sh
@@ -3,7 +3,7 @@
 cd /home/tool/filter
 virtualenv venv # creates a virtualenv in the ./venv directory
 . venv/bin/activate
-pip install -r requirements.txt
+pip install --upgrade -r ../requirements.txt
 pip install nose -I
 deactivate # need to refresh the virtualenv for correct `nose` to be used
 . venv/bin/activate
